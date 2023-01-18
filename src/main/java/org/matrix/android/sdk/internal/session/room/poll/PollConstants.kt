6d7b7b59c0edc6c9c95cd6a34758ce3290ba1f6e
Maxime NATUREL <46314705+mnaturel@users.noreply.github.com>
2023-01-18 14:25:40 +0100

    Loading events in a loop

--- src/main/java/org/matrix/android/sdk/internal/session/room/poll/PollConstants.kt
+++ src/main/java/org/matrix/android/sdk/internal/session/room/poll/PollConstants.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 New Vector Ltd
+ * Copyright (c) 2023 The Matrix.org Foundation C.I.C.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,8 @@
  * limitations under the License.
  */
 
-package org.matrix.android.sdk.api.session.room.poll
+package org.matrix.android.sdk.internal.session.room.poll
 
-/**
- * Represent the status of the loaded polls for a room.
- */
-data class LoadedPollsStatus(
-        val canLoadMore: Boolean,
-        val nbSyncedDays: Int,
-)
+object PollConstants {
+    const val MILLISECONDS_PER_DAY = 24 * 60 * 60_000
+}
